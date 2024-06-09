a10401092c97c1491070bdd6e903d5250b85bd27
Alexander Lunegov <alunegov@gmail.com>
2024-06-09 20:28:50 +0500

    feat: 2 regs as one LE value, increment values for ReadInputRegisters in separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{env, future, thread};
+use std::{env, future, sync::{Arc, Mutex}, thread, time::Duration};
 
 use tokio_modbus::{prelude::*, server::rtu::Server};
 
-struct Service;
+struct Service {
+    n: Arc<Mutex<u16>>,
+}
 
 impl tokio_modbus::server::Service for Service {
     type Request = SlaveRequest<'static>;
@@ -13,16 +15,23 @@ impl tokio_modbus::server::Service for Service {
             Request::ReadHoldingRegisters(addr, qty) => {
                 println!("ReadHoldingRegisters, {addr} {qty}");
                 let mut regs = vec![0; qty.into()];
-                for i in 0..qty {
-                    regs[i as usize] = 2 * i;
+                let mut i = 0;
+                while (i + 2) <= qty {
+                    regs[(i + 0) as usize] = 1;
+                    regs[(i + 1) as usize] = 0;
+                    i += 2;
                 }
                 future::ready(Ok(Response::ReadHoldingRegisters(regs)))
             }
             Request::ReadInputRegisters(addr, qty) => {
                 println!("ReadInputRegisters, {addr} {qty}");
+                let n = self.n.lock().unwrap();
                 let mut regs = vec![0; qty.into()];
-                for i in 0..qty {
-                    regs[i as usize] = i;
+                let mut i = 0;
+                while (i + 2) <= qty {
+                    regs[(i + 0) as usize] = *n;
+                    regs[(i + 1) as usize] = 0;
+                    i += 2;
                 }
                 future::ready(Ok(Response::ReadInputRegisters(regs)))
             }
@@ -43,11 +52,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let builder = tokio_serial::new(port_path.clone(), 115200);
     let server_serial = tokio_serial::SerialStream::open(&builder).unwrap();
 
+    let n = Arc::new(Mutex::new(0));
+
     println!("Starting up server on {port_path}...");
-    let server = thread::spawn(move || {
+    let n1 = n.clone();
+    let server_thread = thread::spawn(move || {
         let rt = tokio::runtime::Runtime::new().unwrap();
         let server = Server::new(server_serial);
-        let service = Service;
+        let service = Service { n: n1 };
         rt.block_on(async {
             if let Err(err) = server.serve_forever(service).await {
                 eprintln!("{err}");
@@ -55,7 +67,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
     });
 
-    let _ = server.join();
+    let n2 = n.clone();
+    let inc_n_thread = thread::spawn(move || {
+        loop {
+            let n = &mut n2.lock().unwrap();
+            **n += 1;
+            thread::sleep(Duration::from_millis(55));
+        }
+    });
+
+    server_thread.join().unwrap();
+    inc_n_thread.join().unwrap();
 
     Ok(())
 }
